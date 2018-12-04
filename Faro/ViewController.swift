c0a660e483d72f90ca9a25b02add5ef2fd884f88
Richard L Zarth III <rlziii@icloud.com>
2018-12-04 00:17:22 -0500

    Initial Commit

---
+++ Faro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Faro
+//
+//  Created by Richard L Zarth III on 2018-12-04.
+//  Copyright © 2018 RLZIII. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
